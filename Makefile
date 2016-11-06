d890089b3328c83c5dc0860179107e1ad4349029
bb2015 <bb2015@ic.ac.uk>
2016-11-06 14:29:15 +0000

    Started implementing visitors

--- Makefile
+++ Makefile
@@ -23,7 +23,7 @@ all: rules
 # runs the antlr build script then attempts to compile all .java files within src
 rules:
 	cd $(ANTLR_DIR) && ./$(ANTLR) 
-	$(FIND) $(SOURCE_DIR) -name '*.java' > $@
+	$(FIND) $(SOURCE_DIR) -name 'WACC*.java' > $@
 	$(MKDIR) $(OUTPUT_DIR)
 	$(JAVAC) $(JFLAGS) @$@
 	$(RM) rules
---
+++ src/Demo.java
@@ -0,0 +1,31 @@
+import antlr.WACCLexer;
+import antlr.WACCParser;
+import antlr.WACCVisitor;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Demo {
+    public static void main(String[] args) throws Exception {
+
+        try {
+            ANTLRInputStream input = new ANTLRInputStream(new FileInputStream("valid/basic/exit/exit-1.wacc"));
+            WACCLexer lexer  = new WACCLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            WACCParser parser = new WACCParser(tokens);
+            //parser.setErrorHandler(new MyErrorStrategy());
+            ParseTree tree = parser.prog();
+
+
+            WACCVisitor visitor = new WACCVisitor();
+            visitor.visit(tree); // need to add thing
+        }catch (IOException e) {
+            System.out.println("Not Accepted");
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
