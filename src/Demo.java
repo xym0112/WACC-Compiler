3d049c55102d08bd322ff19a37e48546c567ba7b
Andreas Asprou <andyasprou@gmail.com>
2016-11-06 17:18:14 +0000

    [AA] Created and Error Listener

--- src/Demo.java
+++ src/Demo.java
@@ -2,29 +2,25 @@ import antlr.WACCLexer;
 import antlr.WACCParser;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Demo {
     public static void main(String[] args) throws Exception {
-
         try {
-            ANTLRInputStream input = new ANTLRInputStream(new FileInputStream("valid/if/if1.wacc"));
+            ANTLRInputStream input = new ANTLRInputStream(new FileInputStream("valid/if/if3.wacc"));
             WACCLexer lexer  = new WACCLexer(input);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             WACCParser parser = new WACCParser(tokens);
             parser.removeErrorListeners();
-//            parser.addErrorListener(new ErrorListener());
-            //parser.setErrorHandler(new MyErrorStrategy());
+            parser.addErrorListener(new ErrorListener());
             parser.prog();
 
-//
 //            WACCVisitor visitor = new WACCVisitor();
 //            visitor.visit(tree); // need to add thing
         } catch (IOException e) {
             System.out.println("Not Accepted");
-        } catch(IllegalArgumentException e){
+        } catch(IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
---
+++ src/ErrorListener.java
@@ -0,0 +1,35 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.misc.Nullable;
+
+public class ErrorListener extends ConsoleErrorListener {
+    @Override
+    public void syntaxError(@NotNull Recognizer<?, ?> recognizer, @Nullable Object offendingSymbol, int line, int charPositionInLine, @NotNull String msg, @Nullable RecognitionException e) {
+        underlineError(recognizer,(Token)offendingSymbol,
+                line, charPositionInLine);
+        System.exit(100);
+    }
+
+    protected void underlineError(Recognizer recognizer,
+                                  Token offendingToken, int line,
+                                  int charPositionInLine) {
+        System.err.println("Syntax error at line " + line + " and position " + charPositionInLine + ":");
+        CommonTokenStream tokens =
+                (CommonTokenStream)recognizer.getInputStream();
+        String input = tokens.getTokenSource().getInputStream().toString();
+        String[] lines = input.split("\n");
+        String errorLine = lines[line - 1];
+        System.err.println("line " + (line - 2) + " " + lines[line - 2]);
+        System.err.println("line " + (line - 1) + " " + errorLine);
+        int offset = 6 + String.valueOf(line - 1).length();
+        for (int i=0; i<charPositionInLine + offset; i++) System.err.print(" ");
+        int start = offendingToken.getStartIndex();
+        int stop = offendingToken.getStopIndex();
+        if ( start>=0 && stop>=0 ) {
+            for (int i=start; i<=stop; i++) System.err.print("^");
+        }
+        System.err.println("");
+        System.err.println("line " + line + " " + lines[line]);
+        System.err.println();
+    }
+}
